8e94b54b21e5947e6705006ade37edd4c82b5351
Victor Vergara <victorjos2001@hotmail.com>
2026-05-28 14:42:21 -0500

    feat: wrap app with ToastProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastProvider } from './components/Toast';
 import Lista from './pages/Lista';
 import Factura from './pages/Factura';
 import Verify from './pages/Verify';
@@ -7,14 +8,16 @@ import EditarFactura from './pages/EditarFactura';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Lista />} />
-        <Route path="/nueva" element={<NuevaFactura />} />
-        <Route path="/factura/:id" element={<Factura />} />
-        <Route path="/factura/:id/editar" element={<EditarFactura />} />
-        <Route path="/verify/:codigo" element={<Verify />} />
-      </Routes>
-    </BrowserRouter>
+    <ToastProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Lista />} />
+          <Route path="/nueva" element={<NuevaFactura />} />
+          <Route path="/factura/:id" element={<Factura />} />
+          <Route path="/factura/:id/editar" element={<EditarFactura />} />
+          <Route path="/verify/:codigo" element={<Verify />} />
+        </Routes>
+      </BrowserRouter>
+    </ToastProvider>
   );
 }
